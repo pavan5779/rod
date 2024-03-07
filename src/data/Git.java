a5aaafe31e8393e42e362582243a9f0e0d998446
BURUNOLLA BALAAKASH <120199192+Balaakash56@users.noreply.github.com>
2024-03-07 17:49:39 +0530

    Update Git.java
    check once

--- src/data/Git.java
+++ src/data/Git.java
@@ -9,6 +9,40 @@ public class Git {
 			System.out.println("eat something ...............");
 		else {
 			System.out.println("Do work hard............");
+			
 	}
 }
+}
+
+
+===========from here =============
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+
+    // Constructor
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Getter methods
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Camry", 2022);
+        System.out.println("My car: " + myCar.getMake() + " " + myCar.getModel() + " (" + myCar.getYear() + ")");
+    }
 }
